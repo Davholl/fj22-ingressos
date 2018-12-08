70d5f8771f72c1994cad4079916b0b670df67e6d
Davi Hollanda <dav-holl@hotmail.com>
2018-12-07 22:46:26 -0200

    Colocando preco na sala, filme e sessao e modificando seus jsps

--- src/main/java/br/com/caelum/ingresso/model/Filme.java
+++ src/main/java/br/com/caelum/ingresso/model/Filme.java
@@ -1,9 +1,12 @@
 package br.com.caelum.ingresso.model;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.Duration;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import java.time.Duration;
 
 /**
  * Created by nando on 03/03/17.
@@ -18,13 +21,20 @@ public class Filme {
     private Duration duracao;
     private String genero;
     
-    
+    private BigDecimal preco;
     
     /**
      * @deprecated hibernate only
      */
     public Filme() {
 
+    }
+    
+    public Filme(String nome, Duration duracao, String genero, BigDecimal preco){
+    	this.nome = nome;
+		this.duracao = duracao;
+		this.genero = genero;
+		this.preco = preco;
     }
     
     public Filme(String nome, Duration duracao, String genero) {
@@ -63,5 +73,13 @@ public class Filme {
 
     public void setGenero(String genero) {
         this.genero = genero;
+    }
+    
+    public BigDecimal getPreco(){
+    	return preco.setScale(2, RoundingMode.HALF_UP);
+    }
+    
+    public void setPreco(BigDecimal preco){
+    	this.preco = preco;
     }
 }
